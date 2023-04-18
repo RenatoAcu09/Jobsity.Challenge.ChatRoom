60630a99ee3b5be84724f713b1120f93c593ac46
racuna <racuna@5CD229DT8Z>
2023-04-17 20:48:51 -0500

    fix migration

--- src/Jobsity.Challenge.ChatRoom.Infra/Migrations/20230416203940_Create.Designer.cs
+++ src/Jobsity.Challenge.ChatRoom.Infra/Migrations/20230416203940_Create.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Jobsity.Challenge.ChatRoom.Infra.Migrations
 {
     [DbContext(typeof(ChatContext))]
-    [Migration("20230416203930_Create")]
+    [Migration("20230416203940_Create")]
     partial class Create
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
